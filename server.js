d10c180e24f4d12c1a10507ae848604525437f7c
art-bbros <art@bbros.eu>
2026-03-13 11:46:26 +0100

    Fix server issue

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { testConnection } = require('./src/config/db');
 const { errorHandler, notFoundHandler } = require('./src/middleware/error.middleware');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // ---------------------------------------------------------------------------
 // Security & parsing middleware
